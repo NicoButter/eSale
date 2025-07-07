02c30a67ad15670c9d571f74a41963c01bac3ce0
Nicolás Butterfield <nicobutter@gmail.com>
2025-07-07 10:30:26 -0300

    se agrego landing page

--- eSale/urls.py
+++ eSale/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from core.views import home_view
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
-    path('', include('store.urls')),
     path('accounts/', include('accounts.urls')),
     path('dashboards/', include('dashboards.urls')),
+    path('store/', include('store.urls')),
 ]
