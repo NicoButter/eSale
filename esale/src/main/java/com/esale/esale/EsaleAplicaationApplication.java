676847ba61b5249401848d1677f41fe96b8143aa
Nicolás Butterfield <nicobutter@gmail.com>
2025-07-08 11:22:07 -0300

    se migro a spring boot

---
+++ esale/src/main/java/com/esale/esale/EsaleAplicaationApplication.java
@@ -0,0 +1,13 @@
+package com.esale.esale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsaleAplicaationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsaleAplicaationApplication.class, args);
+	}
+
+}
---
+++ esale/src/test/java/com/esale/esale/EsaleAplicaationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esale.esale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsaleAplicaationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
