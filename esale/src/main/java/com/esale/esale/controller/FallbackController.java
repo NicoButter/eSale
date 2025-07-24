05327b32e0c10d81cfc0603c882231d27ab0b001
Nicolás Butterfield <nicobutter@gmail.com>
2025-07-24 16:11:36 -0300

    funcionando, ahora a trabajar en el login

--- esale/src/main/java/com/esale/esale/controller/FallbackController.java
+++ esale/src/main/java/com/esale/esale/controller/FallbackController.java
@@ -1,17 +1,12 @@
 package com.esale.esale.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FallbackController {
 
-    @GetMapping("/{path:^(?!api|static|webjars|favicon\\.ico).*$}")
-    public String redirect() {
-        return "forward:/index.html";
-    }
-
-    @GetMapping("/**/{path:[^\\.]*}")
+    @RequestMapping(value = { "/{path:[^\\.]*}" })
     public String forward() {
         return "forward:/index.html";
     }
